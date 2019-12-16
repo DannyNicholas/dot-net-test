709fa792b3dd56e74e3d460d8ff472565c2cd73f
Danny Nicholas <dannynicholas23@gmail.com>
2019-12-16 08:48:30 +0000

    initial commit

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "London", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Dan's Curries", Location = "Manchester", Cuisine=CuisineType.Indian},
+                new Restaurant { Id = 3, Name = "Pedros", Location = "Norwich", Cuisine = CuisineType.Mexican}
+            };
+        }
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                  where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                  orderby r.Name
+                  select r;
+        }
+    }
+}
---
+++ Shuffle/Shuffle/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var startingDeck = from s in Suits()
+            //                   from r in Ranks()
+            //                   select new { Suit = s, Rank = r };
+            var startingDeck = Suits().SelectMany(
+                suit => Ranks().Select(
+                    rank => new { Suit = suit, Rank = rank }));
+
+            // display each card we've generated
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine(card);
+            }
+
+            var shuffledDeck = startingDeck;
+            var times = 0;
+            do
+            {
+                // shuffle cards by interleaving top 26 cards with bottom 26 cards
+                shuffledDeck = shuffledDeck.Take(26).InterleaveSequenceWith(shuffledDeck.Skip(26));
+                times++;
+
+            } while (!startingDeck.SequenceEquals(shuffledDeck));
+
+            Console.WriteLine("Shuffled!");
+            Console.WriteLine(times);
+        }
+
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+
+
+
+
+    }
+}
